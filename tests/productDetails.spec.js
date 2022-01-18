27bae11bb7a6f0e55918661d525857036f5774bd
danillo <danillogonvalves001@gmail.com>
2022-01-18 10:20:44 -0300

    Colocando novamente os comentarios de codigos de teste.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,18 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     const result = productDetails('Alcool gel', 'Máscara');
+    // Teste se productDetails é uma função.
     expect(typeof productDetails).toBe('function');
+    // Teste se o retorno da função é um array.
     expect(typeof result).toBe('object');
+    // Teste se o array retornado pela função contém dois itens dentro.
     expect(Object.keys(result).length).toBe(2);
+    // Teste se os dois itens dentro do array retornado pela função são objetos.
     expect(typeof Object.values(result)[0]).toBe('object');
     expect(typeof Object.values(result)[1]).toBe('object');
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     expect(result[0].name !== result[1].name).toBe(true);
+    // Teste se os dois productIds terminam com 123.
     expect(Object.values(result)[0].details.productId.slice(-3)).toBe('123');
     expect(Object.values(result)[1].details.productId.slice(-3)).toBe('123');
   });
