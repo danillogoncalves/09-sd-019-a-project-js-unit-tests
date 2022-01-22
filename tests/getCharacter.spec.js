42952b09e8860a50c329cac30de958d62d947bfb
Danillo Gonçalves Batista <73307118+danillogoncalves@users.noreply.github.com>
2022-01-22 16:59:11 -0300

    Update tests/getCharacter.spec.js
    Co-authored-by: Luá Octaviano <71564550+luacomacento@users.noreply.github.com>

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -45,7 +45,7 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    expect(getCharacter()).toBe(undefined);
+    expect(getCharacter()).toBeUndefined();
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
     expect(getCharacter('Arya')).toEqual({ name: 'Arya Stark', class: 'Rogue', phrases: [ 'Not today', 'A girl has no name.' ] });
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
